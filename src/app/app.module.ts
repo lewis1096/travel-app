5b2d4a1895d517fbd7998a8219e9bdfccc680cd3
luigialecce <luigi.alecce@gmail.com>
2020-11-01 20:51:35 -0600

    k

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      AboutComponent
+   ],
   imports: [
     BrowserModule
   ],
